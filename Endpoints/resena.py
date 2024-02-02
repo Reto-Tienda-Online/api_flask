9c8508e5fdcf15a5bb2057341510a67b9d8bfa9c
thenetbeangang <Caorey13@hotmail.com>
2024-02-02 09:22:54 +0100

    asdasdasdasd

--- Endpoints/resena.py
+++ Endpoints/resena.py
@@ -8,28 +8,58 @@ resena_bp = APIRouter()
 
 @resena_bp.get("/resena/{juego_id}")
 async def read_resena(juego_id: int, db: Session = Depends(get_db)):
-    resena = (
+    resenas = (
         db.query(Resena)
         .join(Usuario, Resena.id_usuario == Usuario.id)
-        .options(joinedload(Resena.usuario)) 
+        .options(joinedload(Resena.usuario))
         .filter(Resena.id_juego == juego_id)
+        .all()
+    )
+
+    if not resenas:
+        raise HTTPException(status_code=404, detail="Resenas not found for the specified juego_id")
+
+    resenas_list = []
+    for resena in resenas:
+        usuario_nombre = resena.usuario.nombre if resena.usuario else None
+
+        resena_info = {
+            'id': resena.id,
+            'id_juego': resena.id_juego,
+            'id_usuario': resena.id_usuario,
+            'nombre_usuario': usuario_nombre,
+            'valoracion': resena.valoracion,
+            'titulo': resena.resena,
+            'comentario': resena.contenido
+        }
+
+        resenas_list.append(resena_info)
+
+    return resenas_list
+    
+    
+@resena_bp.get("/highest_valoracion/{juego_id}")
+async def get_highest_valoracion(juego_id: int, db: Session = Depends(get_db)):
+    highest_valoracion_resena = (
+        db.query(Resena)
+        .filter(Resena.id_juego == juego_id)
+        .order_by(Resena.valoracion.desc())
         .first()
     )
 
-    if resena is None:
-        raise HTTPException(status_code=404, detail="Resena not found")
+    if highest_valoracion_resena is None:
+        raise HTTPException(status_code=404, detail="No reviews found for the specified juego_id")
 
-    usuario_nombre = resena.usuario.nombre if resena.usuario else None
+    usuario_nombre = highest_valoracion_resena.usuario.nombre if highest_valoracion_resena.usuario else None
 
     return {
-        'id': resena.id,
-        'id_juego': resena.id_juego,
-        'id_usuario': resena.id_usuario,
+        'id': highest_valoracion_resena.id,
+        'id_juego': highest_valoracion_resena.id_juego,
+        'id_usuario': highest_valoracion_resena.id_usuario,
         'nombre_usuario': usuario_nombre,
-        'valoracion': resena.valoracion,
-        'titulo':resena.resena,
-        'comentario': resena.contenido
-        
+        'valoracion': highest_valoracion_resena.valoracion,
+        'titulo': highest_valoracion_resena.resena,
+        'comentario': highest_valoracion_resena.contenido
     }
 
 
